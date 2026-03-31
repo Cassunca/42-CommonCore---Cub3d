c1aaf749fa80255aea1c738ac818deb772d678d1
Christian Sotelo Assuncao <cassunca@c1r8p1.42sp.org.br>
2026-03-31 17:53:59 -0300

    Engine first tests

--- src/engine/render.c
+++ src/engine/render.c
@@ -6,8 +6,39 @@
 /*   By: cassunca <cassunca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 09:01:45 by cassunca          #+#    #+#             */
-/*   Updated: 2026/03/04 09:41:03 by cassunca         ###   ########.fr       */
+/*   Updated: 2026/03/31 14:07:17 by cassunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+	
+	if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT)
+		return ;
+	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	render_background(t_data *data)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			if (y < HEIGHT / 2)
+				my_mlx_pixel_put(&data->img, x, y, 0x87CEEB);
+			else
+				my_mlx_pixel_put(&data->img, x, y, 0x8B4513);
+			x++;
+		}
+		y++;
+	}
+}
