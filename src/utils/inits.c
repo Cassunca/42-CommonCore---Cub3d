11112c2a753ed9a52565157208975aaf2b68c016
amyyrodri <yguuuto@gmail.com>
2026-04-02 22:33:18 -0300

    feat: create buttons start and quit - init leaf - loads imagens

--- src/utils/inits.c
+++ src/utils/inits.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   inits.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 21:34:36 by kamys             #+#    #+#             */
-/*   Updated: 2026/04/01 18:01:53 by amyrodri         ###   ########.fr       */
+/*   Updated: 2026/04/02 22:14:09 by kamys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,28 @@ static t_img	load_image(void *mlx, char *path)
 	return (img);
 }
 
+void	load_array(t_data *game, char *mid_path,
+	t_img *frames, int num_frames)
+{
+	int		i;
+	char	*path;
+	char	*num;
+	char	*tmp;
+
+	i = 0;
+	while (i < num_frames)
+	{
+		num = ft_itoa(i);
+		tmp = ft_strjoin(mid_path, num);
+		free(num);
+		path = ft_strjoin(tmp, ".xpm");
+		free(tmp);
+		frames[i] = load_image(game->mlx, path);
+		free(path);
+		i++;
+	}
+}
+
 static t_bool	load_sprites(t_data *game)
 {
 	game->tex.ea = load_image(game->mlx, game->tex_path.ea);
@@ -61,7 +83,60 @@ static t_bool	load_sprites(t_data *game)
 	game->tex.we = load_image(game->mlx, game->tex_path.we);
 	if (!game->tex.we.ptr)
 		return (FALSE);
+	game->imagem_start = load_image(game->mlx, "assets/start.xpm");
+	game->imagem_quit = load_image(game->mlx, "assets/quit.xpm");
+	game->logo = load_image(game->mlx, "assets/cub3d_logo.xpm");
+	game->logo_42 = load_image(game->mlx, "assets/42_logo.xpm");
+	load_array(game, "assets/leaves/leaf_", game->leaf_frames, MAX_LEAVES_SPRITES);
 	return (TRUE);
+}
+
+void	init_leaf(t_leaf *l)
+{
+	l->x = rand() % WIN_WIDTH;
+	l->y = -(rand() % WIN_HEIGHT);
+
+	l->prev_x = l->x;
+	l->prev_y = l->y;
+
+	l->speed_y = 30 + rand() % 40;
+
+	l->amplitude = 20 + rand() % 30;
+	l->frequency = 1 + (rand() % 100) / 50.0;
+
+	l->time = rand() % 100;
+
+	l->frame = rand() % 5;
+	l->frame_dir = (rand() % 2) ? 1 : -1;
+	l->anim_speed = 0.2;
+	l->anim_time = 0;
+}
+
+void	play_action(void *param)
+{
+	t_data *game = (t_data *)param;
+	printf("play\n");
+	game->screen = GAME;
+}
+
+void	quit_action(void *param)
+{
+	(void)param;
+	close_window(param);
+	exit(0);
+}
+
+t_button	create_button(int x, int y, t_img *img,
+	void (*on_click)(void *))
+{
+	t_button	btn;
+
+	btn.x = x;
+	btn.y = y;
+	btn.img = img;
+	btn.on_click = on_click;
+	btn.is_hover = FALSE;
+	return (btn);
 }
 
 t_bool	init_game(t_data *game)
@@ -76,6 +151,16 @@ t_bool	init_game(t_data *game)
 	game->win = mlx_new_window(game->mlx, WIN_WIDTH, WIN_HEIGHT, "CUBO");
 	if (!game->win)
 		return (FALSE);
+	for (int i = 0; i < MAX_LEAVES; i++)
+		init_leaf(&game->leaves[i]);
+
+	int y = (game->frame.height / 2);
+	int x = (game->frame.width / 2);
+	game->btn[0] = create_button(x - (game->imagem_start.width / 2), y + 100,
+	&game->imagem_start, play_action);
+
+	game->btn[1] = create_button(x - (game->imagem_quit.width / 2), y + 150,
+		&game->imagem_quit, quit_action);
 	game->screen = TITLE;
 	return (TRUE);
 }
